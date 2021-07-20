99eec62d0e25e247c36e1711500d09ede35cec09
Lane Pemberton <lanebpemberton@gmail.com>
2021-07-20 01:22:47 -0400

    added boiler plate server code to import and connect apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//import schema
+const { typeDefs, resolvers } = require('./schemas');
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  //add auth middleware to server to authenticate calls
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
